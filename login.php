7f9db9a56e3f6fbd60a0286bc35b44063eeeaedd
Ismael <ismael_iem@hotmail.com>
2025-09-25 13:45:47 +0200

    datos de conexión a BBDD

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
 // Parámetros de conexión
 $host = "localhost";
-$usuario_db = "root";
-$contrasena_db = "";
-$nombre_db = "mi_base_de_datos";
+$usuario_db = "admin";
+$contrasena_db = "123456";
+$nombre_db = "login";
 
 // Crear conexión
 $conn = new mysqli($host, $usuario_db, $contrasena_db, $nombre_db);
@@ -18,7 +18,7 @@ $usuario = $_POST['usuario'];
 $contrasena = $_POST['contrasena'];
 
 // Preparar consulta segura
-$sql = "SELECT * FROM usuarios WHERE usuario = ? AND contrasena = ?";
+$sql = "SELECT * FROM users WHERE user = ? AND password = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("ss", $usuario, $contrasena);
 $stmt->execute();
